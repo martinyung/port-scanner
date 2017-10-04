32380f6c5d88b0eda0a44d98cc2b04f809a4b13d
Yung Chun Ern Martin <martinyce@gmail.com>
2017-10-04 22:44:41 +0800

    add multi thread for concurrent nmap scans

--- scanner-api/main.py
+++ scanner-api/main.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
 from os import *
+import queue
+import threading
+
+queue = queue.Queue(maxsize=0)
 
 app = Flask(__name__)
 CORS(app)
@@ -8,15 +12,20 @@ CORS(app)
 @app.route('/')
 def index():
 	target = 'vulnweb.com'
-	scanner1 = scanner('-PS -F', target)
-	scanner2 = scanner('-PA -F', target)
-	return scanner1 + '--------\n' + scanner2
 
-def scanner(setting, target):
+	scanner1 = threading.Thread(scanner('-PS -F', target, queue))
+	scanner2 = threading.Thread(scanner('-PA -F', target, queue))
+
+	scanner1.start()
+	scanner2.start()
+
+	return queue.get_nowait()
+
+def scanner(setting, target, queue):
 	command = 'nmap ' + setting + ' ' + target
 	process = popen(command)
 	results = str(process.read())
-	return results
+	queue.put(results)
 
 if __name__ == '__main__':
 	app.run(debug=True)
